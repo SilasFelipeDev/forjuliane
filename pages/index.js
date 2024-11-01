1b97e6e4eab6fd569bb70604e1b7564ff2f9c5cf
SilasFelipeDev <devsilasfelipe@gmail.com>
2024-11-01 21:37:35 +0000

    commit inicial, uma mensagem de carinho

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+function Home(){
+    return (
+        <div>
+            <h1>Para Juliane</h1>
+            <p>Que as dificuldades da vida nunca sejam 
+                maiores que a força que nos move: o AMOR
+            </p>
+            <h2>Eu sempre estarei aqui para você</h2>
+        </div>
+    );
+}
+
+
+export default Home;
